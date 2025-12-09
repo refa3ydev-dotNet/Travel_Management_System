90b177d2013a4ad6a68214f0071df432f0d6c0c4
Omar Refaey <refa3y.dev@gmail.com>
2025-12-09 19:15:51 +0200

    TMS Project

---
+++ src/tms/model/Hostel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tms.model;
+
+import tms.model.Accommodation;
+
+/**
+ *
+ * @author 3mora
+ */
+public class Hostel extends Accommodation{
+    
+}
---
+++ src/tms/model/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tms.model;
+
+/**
+ *
+ * @author 3mora
+ */
+public class User {
+    
+}
